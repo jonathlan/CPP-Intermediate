a70207d63a6fa8eb14a8c64508f3b53f54db6838
jonathlan <38994980+jonathlan@users.noreply.github.com>
2020-05-05 17:06:09 +0200

    Add constructors example using a new class: Person

--- CPP-Classes/CPP-Classes/CPP-Classes.cpp
+++ CPP-Classes/CPP-Classes/CPP-Classes.cpp
@@ -2,11 +2,22 @@
 
 #include <iostream>
 #include "Math.h"
+#include "Person.h"
 
 using namespace std;
 
 int main()
 {
+    cout << "STATIC CLASS" << endl;
     int result = Math::pow(1    , 10);
     cout << result << endl;
+
+    cout << "USING CONSTRUCTORS" << endl;
+    Person* pOne = new Person();
+    Person* pTwo = new Person("John", "Doe");
+    Person* pThree = new Person("Jane", "Jason", 24);
+
+    delete pOne, pTwo, pThree;
+
+    return 0;
 }
