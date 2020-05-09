7567511651599a12dd7ab83ead3a386bd4f4d2ab
jonathlan <38994980+jonathlan@users.noreply.github.com>
2020-05-09 23:52:22 +0200

    Add properties for Student class

--- Lab/Student.cpp
+++ Lab/Student.cpp
@@ -1,1 +1,84 @@
 #include "Student.h"
+#include <regex>
+
+string Student::get_firstname()
+{
+	return this->firstName;
+}
+
+void Student::set_firstname(string fname)
+{
+	if (!fname.empty() and !fname.compare(NULL))
+		this->firstName = fname;
+	else
+		cout << "Enter a valid value";
+}
+
+string Student::get_lastname()
+{
+	return this->lastName;
+}
+
+void Student::set_lastname(string lname)
+{
+	if (!lname.empty() and !lname.compare(NULL))
+		this->lastName = lname;
+	else
+		cout << "Enter a valid value";
+}
+
+int Student::get_age()
+{
+	return this->age;
+}
+
+void Student::set_age(int age)
+{
+	if (age > 0)
+		this->age = age;
+	else
+		cout << "Enter a valid value";
+}
+
+string Student::get_address()
+{
+	return this->address;
+}
+
+void Student::set_address(string add)
+{
+	if (!add.empty() and !add.compare(NULL))
+		this->address = add;
+	else
+		cout << "Enter a valid value";
+}
+
+string Student::get_city()
+{
+	return string();
+}
+
+void Student::set_city(string city)
+{
+	if (!city.empty() and !city.compare(NULL))
+		this->city = city;
+	else
+		cout << "Enter a valid value";
+}
+
+string Student::get_phone()
+{
+	return this->phone;
+}
+
+void Student::set_phone(string phone)
+{
+	std::regex r("[[:digit:]]{2}-[[:digit:]]{3}-[[:digit:]]{4}");
+	regex* reg = new regex();
+	
+	
+	if (std::regex_match(phone, r))
+		this->phone = phone;
+	else
+		cout << "Enter a valid value";
+}
--- Lab/Student.h
+++ Lab/Student.h
@@ -10,5 +10,18 @@ private:
 	int age = 0;
 	string address = "";
 	string city = "";
-	double phone = 0;
+	string phone = 0;
+public:
+	string get_firstname();
+	void set_firstname(string fname);
+	string get_lastname();
+	void set_lastname(string lname);
+	int get_age();
+	void set_age(int age);
+	string get_address();
+	void set_address(string add);
+	string get_city();
+	void set_city(string city);
+	string get_phone();
+	void set_phone(string phone);
 };
