952f34b364d3c4a03056d376e18f3ec16b4a6e37
jonathlan <38994980+jonathlan@users.noreply.github.com>
2020-05-10 20:15:40 +0200

    Add accessors for Teacher class

--- Lab/Teacher.cpp
+++ Lab/Teacher.cpp
@@ -1,1 +1,93 @@
 #include "Teacher.h"
+#include <regex>
+
+Teacher::Teacher()
+{
+}
+
+Teacher::~Teacher()
+{
+}
+
+string Teacher::get_firstname()
+{
+	return this->firstName;
+}
+
+void Teacher::set_firstname(string fname)
+{
+	if (!fname.empty())
+		this->firstName = fname;
+	else
+		cout << "Enter a valid value" << endl;
+}
+
+string Teacher::get_lastname()
+{
+	return this->lastName;
+}
+
+void Teacher::set_lastname(string lname)
+{
+	if (!lname.empty())
+		this->lastName = lname;
+	else
+		cout << "Enter a valid value" << endl;
+}
+
+int Teacher::get_age()
+{
+	return this->age;
+}
+
+void Teacher::set_age(int age)
+{
+	if (age > 0)
+		this->age = age;
+	else
+		cout << "Enter a valid value" << endl;
+}
+
+string Teacher::get_address()
+{
+	return this->address;
+}
+
+void Teacher::set_address(string add)
+{
+	if (!add.empty())
+		this->address = add;
+	else
+		cout << "Enter a valid value" << endl;
+}
+
+string Teacher::get_city()
+{
+	return string();
+}
+
+void Teacher::set_city(string city)
+{
+	if (!city.empty())
+		this->city = city;
+	else
+		cout << "Enter a valid value" << endl;
+}
+
+string Teacher::get_phone()
+{
+	return this->phone;
+}
+
+void Teacher::set_phone(string phone)
+{
+	std::regex r("[[:digit:]]{2}-[[:digit:]]{3}-[[:digit:]]{4}");
+	//ex. 55-328-0183
+	regex* reg = new regex();
+
+
+	if (std::regex_match(phone, r))
+		this->phone = phone;
+	else
+		cout << "Enter a valid value" << endl;
+}
--- Lab/Teacher.h
+++ Lab/Teacher.h
@@ -1,5 +1,30 @@
 #pragma once
+#include <iostream>
+using namespace std;
+
 class Teacher
 {
+private:
+	string firstName = "";
+	string lastName = "";
+	int age = 0;
+	string address = "";
+	string city = "";
+	string phone = "";
+public:
+	Teacher();
+	~Teacher();
+	string get_firstname();
+	void set_firstname(string fname);
+	string get_lastname();
+	void set_lastname(string lname);
+	int get_age();
+	void set_age(int age);
+	string get_address();
+	void set_address(string add);
+	string get_city();
+	void set_city(string city);
+	string get_phone();
+	void set_phone(string phone);
 };
 
